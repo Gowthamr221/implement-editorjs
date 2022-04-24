8fc39db3809d6a6bbc2453e1011fa2a4c70be4b5
Gowthamr221 <gowthamr221@gmail.com>
2022-04-25 02:41:07 +0530

    learn to implement editorjs

---
+++ index.js
@@ -0,0 +1,11 @@
+import EditorJS from "@editorjs/editorjs";
+import { Header } from "@editorjs/header";
+import { List } from "@editorjs/list";
+import { Embed } from "@editorjs/embed";
+const editor = new EditorJS({
+    holderId : 'editorjs',
+    tools:{
+    
+
+    }
+})
